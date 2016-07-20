4dfed92e8f9e85351aea343627c7dedc90677b02
BladeRunner777 <johnnyshark1@gmail.com>
2016-07-20 16:33:14 +0300

    CardController2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'cards/index'
-
   # get 'cards_controller/index'
   # get 'welcome/index'
-  resources:cards
-  
+  resources :cards
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
