934fd40b2646c3337af9995ddd2282a481900238
BladeRunner777 <johnnyshark1@gmail.com>
2016-08-11 18:32:57 +0300

    Nokogiri

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#для получения контента через http
+require 'open-uri'
+
+#подключаем Nokogiri
+require 'nokogiri'
+
+page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
+#p page.css('body')
+
+50.times do |index|
+  index = index + 2
+  css = '#jsn-mainbody > div.com-content > div > div.jsn-article-content > table > tbody > tr:nth-child(' + index.to_s + ') > td.bigLetter'
+  css2 = '#jsn-mainbody > div.com-content > div > div.jsn-article-content > table > tbody > tr:nth-child(' + index.to_s + ') > td:nth-child(3)'
+
+  p index
+  p page.css(css).text
+  p page.css(css2).text
+
+  Card.create!([{
+  original_text: page.css(css).text,
+  translated_text: page.css(css2).text
+  }])
+end
+
+=begin
+Card.create!([{
+  review_date: 3.days.ago,
+  original_text: "Eugene",
+  translated_text: "Женя"
+  }])
+p "We have #{Card.count} cards in data base!"
+=end
